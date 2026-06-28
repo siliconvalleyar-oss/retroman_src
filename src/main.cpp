0638e9c5f7e0394c718e4c8487edb1de661971fb
siliconvalleyar-oss <siliconvalleyar@gmail.com>
2026-06-28 15:43:37 -0300

    refactor: reorganize project structure and clean up Makefile
    - Move main.cpp into src/ directory
    - Rename src/linux/ to src/tinyPTC/ (proper library naming)
    - Rewrite Makefile: fix patsubst typo, clean variable names, add cleanall target
    - Remove unused placeholder files (dir1/, mfile/)
    - Bump version to 1.2.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 extern "C" {
-    #include "src/linux/tinyptc.h"
+	#include "tinyPTC/tinyptc.h"
 }
 
 #include <iostream>
-//#define __PTC_MMX__ 
-//#include "src/linux/xshm.h"
-//#include "src/linux/tinyptc.h"
-
 
 using namespace std;
 
@@ -15,6 +11,6 @@ int main(){
 //ptc_open("Juego",640,320);
 //extern int ptc_open(const char *title, int width, int height);
 //ptc_close();
-cout << "my first app game"<<endl;
+//cout << "my first app game"<<endl;
 return 0;
 }
